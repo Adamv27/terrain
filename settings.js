6b6e77aab9a815f6ded00b63e6b18fdb7cf21bb2
Adam Vinch <adamvinch@gmail.com>
2024-02-12 01:42:39 -0500

    Restart project

---
+++ settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  WIDTH: 800,
+  HEIGHT: 450,
+  TILE_SIZE: 10
+};
+
+export { settings };
---
+++ terrain.js
@@ -0,0 +1,60 @@
+import { settings } from './settings.js'
+import { noise } from './perlin.js'
+
+const canvas = document.getElementById('canvas');
+canvas.width = settings.WIDTH;
+canvas.height = settings.HEIGHT;
+const ctx = canvas.getContext('2d');
+
+
+const setUp = () => {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(10, 10, 1000, 1000);
+  
+  const rows = settings.HEIGHT / settings.TILE_SIZE;
+  const columns = settings.WIDTH / settings.TILE_SIZE;
+  
+  noise.seed(Math.random());
+
+  const grid = Array.from(Array(rows), () => new Array(columns).fill(0));
+  const SCALE = 0.1;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      let value = noise.perlin2(i * SCALE, j * SCALE);
+      grid[i][j] = value;
+    }
+  }
+  console.log(grid);
+  drawGrid(grid);
+}
+
+
+const setCtx = value => {
+  if (value >= 0.1) {
+    ctx.fillStyle = 'green' 
+  } else if (value >= -0.08) {
+    ctx.fillStyle = '#C2B280'
+  } else if (value >= -0.15){
+    ctx.fillStyle = '#51ACC2'
+  } else {
+    ctx.fillStyle = '#006992'
+  }
+}
+
+const drawGrid = grid => {
+  const colors = ['blue', 'green', 'red'];
+
+  grid.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      setCtx(column)
+      const x = columnIndex * settings.TILE_SIZE
+      const y = rowIndex * settings.TILE_SIZE
+      ctx.fillRect(x, y, settings.TILE_SIZE, settings.TILE_SIZE)
+    })
+  })
+}
+
+
+setUp();
+
+
